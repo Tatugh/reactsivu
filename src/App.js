02e8276f529b7b296eb2edad77e5a21cd2576e07
Tatugh <tatu.ruohoaho@hotmail.fi>
2025-02-14 11:19:58 +0200

    kommitti

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload Halloota.
         </p>
+        <h1>Joo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
